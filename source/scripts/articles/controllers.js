fa0380d4eab80d1fa4b8e889d90630dd7dc784fb
David Woods <dwoods@vertigo.com>
2013-04-16 17:00:48 -0700

    Clickable Tags, and article search moves left.

--- source/scripts/articles/controllers.js
+++ source/scripts/articles/controllers.js
@@ -5,12 +5,27 @@
 function ArticleListController($scope, $http) {
   $http.get('articles.json').success(function(data) {
     var list = [];
+    var tagList = [];
+    
+    // Turn the incoming .json file into a useable array.
     for (var key in data) {
       if (data.hasOwnProperty(key)) {
-        list.push(data[key]);
+        var item = data[key];
+        list.push(item);
+        
+        // Add unique tags to the tag list
+        for (var tag in item.tags) {
+          if (tagList.indexOf(item.tags[tag]) === -1) {
+            tagList.push(item.tags[tag]);
+          }
+        }
       }
     }
     $scope.articles = list;
+    $scope.tags = tagList;
+    $scope.setSearchTag = function(tag) {
+      $scope.searchTag = tag;
+    };
   });
 }
 
